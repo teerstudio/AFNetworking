744657b0ed61cf6853aa3a5787102db98fc4f7a9
Thirawat Phannet <TeeR@Thirawats-MacBook-Pro.local>
2015-02-13 13:49:56 +0700

    Convert to Modern Syntax

--- Example/Classes/Models/Post.m
+++ Example/Classes/Models/Post.m
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
- 
+
 #import "Post.h"
 #import "User.h"
 
@@ -57,7 +57,7 @@
         }
     } failure:^(NSURLSessionDataTask *__unused task, NSError *error) {
         if (block) {
-            block([NSArray array], error);
+            block(@[], error);
         }
     }];
 }
